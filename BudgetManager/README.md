7b9bc49cea24c9a680bca79e988dae5dff34ba8a
Justin Lu <lujp@usc.edu>
2020-07-23 09:37:20 -0700

    Finished Budget Manager
    Added Readme

---
+++ BudgetManager/README.md
@@ -0,0 +1,6 @@
+# Budget Manager Chrome Extension Tutorial by Codevolution
+ [Link](https://www.youtube.com/channel/UC80PWRj_ZU8Zu0HSMNVwKWw)  
+A Google Chrome extension that tracks money spent.  
+Allows a limit to be set, and will create a notification if money spent exceeds the limit.  
+Allows user to track spent money via context menu by highlighting a integer on a webpage.  
+Limit set and amount spent reset via options.
--- BudgetManager/popup.js
+++ BudgetManager/popup.js
@@ -6,7 +6,7 @@ $(function(){
     })
 
     $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total', function(budget){
+        chrome.storage.sync.get(['total', 'limit'], function(budget){
             var newTotal = 0;
             if (budget.total){
                 newTotal += parseInt(budget.total);
@@ -17,10 +17,21 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){
+                if(amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: 'basic',
+                        iconUrl: 'icon48.png',
+                        title: 'Limit reached!',
+                        message: "Uh oh! Looks like you've reached your limit!"
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+                    chrome.notifications.clear('limitNotif');
+                }
+            });
             
             $('#total').text(newTotal);
             $('#amount').val('');
-        })
-    })
+        });
+    });
 });
